c717c23490203270be8833c39af6b72f39d2216c
Paul Bailey <paulba@stedwards.edu>
2016-02-23 16:41:59 -0600

    update params

---
+++ manifests/migrate.pp
@@ -0,0 +1,43 @@
+# == Class bitbucket::migrate
+#
+# This class is called from bitbucket for migrating from Stash.
+#
+class bitbucket::migrate {
+  if $::bitbucket::migrate_from_stash {
+    exec { 'shutdown_stash':
+      command => $::bitbucket::migrate_stop,
+      path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      onlyif  => "/bin/ps -u ${::bitbucket::migrate_user}",
+    }
+
+    exec { 'move_homedir':
+      command => "/bin/mv ${::bitbucket::migrate_homedir} ${::bitbucket::homedir}",
+      path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      creates => $::bitbucket::homedir,
+      onlyif  => "test -d ${::bitbucket::migrate_homedir}",
+      unless  => "/bin/ps -u ${::bitbucket::migrate_user}",
+      require => Exec['shutdown_stash'],
+      before  => File[$::bitbucket::homedir],
+    }
+
+    if $::bitbucket::manage_usr_grp {
+      exec { 'group_migrate':
+        command => "groupmod -n ${::bitbucket::group} ${::bitbucket::migrate_group}",
+        path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+        onlyif  => "cat /etc/groups | grep ^${::bitbucket::migrate_group}",
+        unless  => "/bin/ps -u ${::bitbucket::migrate_user}",
+        require => Exec['shutdown_stash'],
+        before  => Group[$::bitbucket::group],
+      }
+
+      exec { 'user_migrate':
+        command => "usermod -l ${::bitbucket::user} ${::bitbucket::migrate_user}",
+        path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+        onlyif  => "cat /etc/passwd | grep ^${::bitbucket::migrate_user}",
+        unless  => "/bin/ps -u ${::bitbucket::migrate_user}",
+        require => Exec['shutdown_stash'],
+        before  => User[$::bitbucket::user],
+      }
+    }
+  }
+}
